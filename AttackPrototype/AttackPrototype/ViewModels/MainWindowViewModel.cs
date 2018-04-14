17b698bc1d33e641ef36c168dcc77af5b03b905f
DESKTOP-PEPNLPD\pawel <paweltruong@gmail.com>
2018-04-14 13:22:49 +0200

    viewModels

--- AttackPrototype/AttackPrototype/ViewModels/MainWindowViewModel.cs
+++ AttackPrototype/AttackPrototype/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AttackPrototype
+namespace AttackPrototype.ViewModels
 {
     public class MainWindowViewModel
     {
